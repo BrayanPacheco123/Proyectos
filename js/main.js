42fd33f098692981fdb7acdf22eaf970b9998333
BrayanPacheco123 <101464581+BrayanPacheco123@users.noreply.github.com>
2022-03-12 12:00:36 -0600

    Explicacion general JS

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// es un espacio de memoria donde se almacena informacion de la aplicacion
+// una variable es un espacio de memoria donde se almacena informacio
+// declaracion: es cuando se pone un nombre a un espacio de memoria
+let nombre;
+// asignacion: cuando se le asigna un valor a un espacio de memoria
+nombre = 'Juan';
+
+// tipo de dato: string, number, object, array, boolean, null, undefined
+// string: cadena de caracteres
+let apellido='Perez';
+// number: numeros
+let edad= 30;
+// object: objetos viajan a traves de JSON(javascript object notation)
+let persona = { 
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 30
+}
+// array: listas de datos
+let lenguajes = ['JavaScript', 'PHP', 'Python', 2, true, {nombre: 'Juan', apellido: 'Perez'}];
+// boolean: true or false
+let casado= false;
+// undefined: cuando no se le asigna un valor a un espacio de memoria
+
+// Javascript es un lenguaje de programacion orientado a objetos
+// es un lenguaje tipado
+// tipado o no tipado: es necesario o no establecer el tipo de dato que se va a guardar en una variable
+// Ecmas = estandar de uso para javascript
+// Ecmascript = estandar de uso para javascript
+// TypeScript = es la implementacion del estandar de uso para javascript
+// Tipado dinamico de datos 
+
+//let nombre: String;
+
+// Tipos de declaracion de variables en js
+// const: constante, no se puede cambiar el valor de la variable
+// var: variable, se le puede cambiar el valor, es global
+// let: variable, se puede cambiar el valor, es local, existe dentor de una estructurade flujo determinada por un bloque de codigo
